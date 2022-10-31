e995c37ea81caf786774d70c2d5f2059380324df
Areej <Areej-fahad>
2022-10-31 10:26:50 +0300

    HW3

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './comp/Nav'
+
+function App() {
+  return (
+    <>
+      <Nav/>
+      <p>ddddd</p>
+
+    </>
+  );
+}
+
+export default App;
